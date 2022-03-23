404bc79fd83ef1ddd55abd5b6e9c74084832da1b
effect <suchkov93@gmail.com>
2022-03-23 23:33:11 +0300

    17-grid-last_changes

--- 17_grid/js/script.js
+++ 17_grid/js/script.js
@@ -4,6 +4,8 @@ document.querySelector('#burger').addEventListener('click', function () {
 	document.querySelector('#menu').classList.toggle('is-active')
 	document.querySelector('#burger').classList.add('none')
 	document.querySelector('#close').classList.add('is-active')
+	document.querySelector('#header').style.background = '#2F2C45';
+	document.querySelector('#header__img').style.visibility = 'hidden';
 	document.querySelector('body').style.overflow = 'hidden';
 })
 
@@ -11,6 +13,8 @@ document.querySelector('#close').addEventListener('click', function() {
 		document.querySelector('#menu').classList.toggle('is-active')
 		document.querySelector('#close').classList.remove('is-active')
 		document.querySelector('#burger').classList.remove('none')
+		document.querySelector('#header').style.background = '#3F2AFF';
+		document.querySelector('#header__img').style.visibility = 'visible';
 		document.querySelector('body').style.overflow = 'visible';
 	})
 })
